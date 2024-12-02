fa7716e59db8476685e58d4018cf09383cb87d4a
“kaplan81” <gesteira.pro@gmail.com>
2024-12-02 16:09:40 +0100

    add expect location element to contain certain text and awaited

--- tests/m3-core-concepts/03-assertions.test.ts
+++ tests/m3-core-concepts/03-assertions.test.ts
@@ -9,6 +9,7 @@ test('Simple assertions', async ({ page }) => {
 test('Simple auto-retrying assertions', async ({ page }) => {
   const url = 'http://localhost:3000/';
   await page.goto(url);
+  await expect(page.getByTestId('location')).toContainText('New York');
   await expect(page).toHaveTitle('Credit Association');
   await expect(page).toHaveURL(url);
 });
