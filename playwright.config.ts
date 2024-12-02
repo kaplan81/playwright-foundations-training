155375170ef8d598d2f7ae7e82e026e40a0bfe23
“kaplan81” <gesteira.pro@gmail.com>
2024-12-02 16:20:15 +0100

    improve configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from '@playwright/test';
 
+const baseURL = 'http://localhost:3000/';
+
 export default defineConfig({
   testDir: './tests',
   reporter: 'html',
   webServer: {
     command: 'npm start',
-    url: 'http://localhost:3000/',
+    url: baseURL,
+  },
+  use: {
+    baseURL,
+    headless: false,
+    launchOptions: { slowMo: 1000 },
   },
 });
