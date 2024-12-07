8df342daa3a7bd0bb9c6037c46ad86ca25408c1b
“kaplan81” <gesteira.pro@gmail.com>
2024-12-07 23:45:01 +0100

    add tracing

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,6 +22,11 @@ export default defineConfig({
     baseURL,
     headless: false,
     launchOptions: { slowMo: 1000 },
+    /**
+     *  Collect trace when retrying the failed test.
+     * See https://playwright.dev/docs/trace-viewer
+     */
+    trace: 'on-first-retry',
   },
   projects: [
     {
