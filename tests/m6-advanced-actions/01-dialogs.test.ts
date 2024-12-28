4a516a32199a6d65e1c6604261bc57692e3ef473
kaplan81 <gesteira.pro@gmail.com>
2024-12-28 20:08:35 +0100

    add comment

--- tests/m6-advanced-actions/01-dialogs.test.ts
+++ tests/m6-advanced-actions/01-dialogs.test.ts
@@ -7,7 +7,15 @@ test('Dialog default handling is to dismiss', async ({ page }) => {
   const input: Locator = page.getByLabel('First name');
   await input.fill(name);
   await expect(input).toHaveValue(name);
-
+  /**
+   * This will open a dialog. Playwright will dismiss it by default
+   * unless there is a `page.on('dialog')` listener, in which case
+   * we need either to acccept or dismiss it. Otherwise the page
+   * will freezze and the test will never finish.
+   *
+   * That is why, in this case, the first name input is not cleared.
+   * The dialog will open and it will be dismissed automatically.
+   */
   await page.getByRole('button', { name: 'Clear' }).click();
   await expect(input).toHaveValue(name);
 });
